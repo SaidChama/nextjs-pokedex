d8038030c155825e930ae14cb20b37471d3f4906
Said Chama <said.chama@gmail.com>
2021-05-02 17:48:04 -0300

    Type component created

--- pages/pokemon.jsx
+++ pages/pokemon.jsx
@@ -1,9 +1,9 @@
 import Layout from "../components/Layout"
+import Type from "../components/Type"
 import Link from 'next/link'
 import Types from "../styles/types.module.css"
 
 export default function pokemon({ pokemon, abilities, types }) {
-    console.log(types)
     return (
         <Layout title={firstUpper(pokemon.name)}>
             <h1 className="text-center font-semibold text-5xl capitalize">
@@ -12,13 +12,14 @@ export default function pokemon({ pokemon, abilities, types }) {
             <div className="flex">
                 <img src={pokemon.sprites.versions['generation-vii']['ultra-sun-ultra-moon'].front_default} alt="" />
                 <div className="flex flex-col">
-                    {abilities.map((id, index) => (
-                        <li className="capitalize" key={index}>{index + 1}. {removeSpecialChars(id.ability.name)}</li>
+                {abilities.map((id, index) => (
+                        id.is_hidden ?
+                        <li className="capitalize" key={index}>Hidden: {removeSpecialChars(id.ability.name)}</li>                
+                        :
+                        <li className="capitalize" key={index}>Ability {index + 1}: {removeSpecialChars(id.ability.name)}</li>
                     ))}
-                    {types.map((id, index) => (
-                        <div key={index} 
-                        className={[`capitalize rounded-full border border-white text-center 
-                            text-white font-semibold`, Types[id.type.name]].join(' ')}>{removeSpecialChars(id.type.name)}</div>
+                    {types.map((type, index) => (
+                        <Type key={index} type={type}/>
                     ))}
                     <h1>Height: {pokemon.height} ft</h1>
                     <h1>Weight: {pokemon.weight} lb</h1>
@@ -38,11 +39,14 @@ export async function getServerSideProps({ query }) {
     const id = query.id
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const pokemon = await res.json()
+
     const abilities = pokemon.abilities.map((abilities) => {
         return abilities
     })
+
     const types = pokemon.types.map((types) => {
-        return types
+        const type = types.type.name
+        return type
     })
 
     return {
